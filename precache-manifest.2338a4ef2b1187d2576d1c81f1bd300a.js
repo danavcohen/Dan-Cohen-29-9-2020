e01eca10e5f7658b0c6b3df8fd5ad8386332a59b
danavcohen <da_da1@walla.co.il>
2020-10-01 14:51:53 +0300

    Updates

--- precache-manifest.2338a4ef2b1187d2576d1c81f1bd300a.js
+++ precache-manifest.2338a4ef2b1187d2576d1c81f1bd300a.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a304bbb3b948f733e180f6f13905db8e",
+    "revision": "85984ce6151397899a09bbc153b256a3",
     "url": "/Dan-Cohen-29-9-2020/index.html"
   },
   {
-    "revision": "5f34c7eba867dc5c781d",
-    "url": "/Dan-Cohen-29-9-2020/static/css/main.69ad8086.chunk.css"
+    "revision": "e69bd98ec0356dcc50d6",
+    "url": "/Dan-Cohen-29-9-2020/static/css/main.c67af18b.chunk.css"
   },
   {
     "revision": "302b1ac4b69e01428bb5",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Dan-Cohen-29-9-2020/static/js/2.24db8220.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5f34c7eba867dc5c781d",
-    "url": "/Dan-Cohen-29-9-2020/static/js/main.f4d2b2da.chunk.js"
+    "revision": "e69bd98ec0356dcc50d6",
+    "url": "/Dan-Cohen-29-9-2020/static/js/main.5e438ea3.chunk.js"
   },
   {
     "revision": "24218585f7635f39082c",
